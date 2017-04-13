b4fb9087ad4bfb30e4a54f571a27e267afa83778
Jackson Graziano <jackgraziano@gmail.com>
2017-04-13 15:35:54 -0300

    before stylesheets change

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,4 +3,16 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = params[:cocktail_id]
+    @dose.save
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
