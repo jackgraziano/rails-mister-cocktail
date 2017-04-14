a6ad32c0bcebc42a3dbeddf4910b249db33fb63a
Jackson Graziano <jackgraziano@gmail.com>
2017-04-14 14:57:26 -0300

    fixing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,26 @@
 
 
   15.times do |index|
-    current_ingredient_index = "strIngredient" + index.to_s
-    current_measurement_index = "strMeasure" + index.to_s
+    current_ingredient_index = "strIngredient" + (index + 1).to_s
+    current_measurement_index = "strMeasure" + (index + 1).to_s
     current_ingredient = obj["drinks"][0][current_ingredient_index]
+    current_dose = obj["drinks"][0][current_measurement_index]
     if current_ingredient != ""
       @ingredient = Ingredient.new
       @ingredient.name = current_ingredient
-      @ingredient.save
-
-      @dose = Dose.new
-      @dose.ingredient = @ingredient
-      @dose.cocktail = @cocktail
-      @dose.description = obj["drinks"][0][current_measurement_index]
-      @dose.save
+      if @ingredient.save
+        @dose = Dose.new
+        @dose.ingredient = @ingredient
+        @dose.cocktail = @cocktail
+        @dose.description = current_dose
+        @dose.save
+      else
+        @dose = Dose.new
+        @dose.ingredient = Ingredient.find_by_name(current_ingredient)
+        @dose.cocktail = @cocktail
+        @dose.description = current_dose
+        @dose.save
+      end
     end
   end
 end
